4894e9fc1366ea18ccef6c29eb397af5d407902c
Bahodir Sayfiev <bahodirsayfiev9801@gmail.com>
2022-12-01 11:42:59 +0500

    fixed

--- app/Http/Controllers/API/v1/OrderProccessingController.php
+++ app/Http/Controllers/API/v1/OrderProccessingController.php
@@ -9,64 +9,24 @@ class OrderProccessingController extends Controller
 {
     public function get_order_info()
     {
-        $orderObject = '{
-            "success": 0,
-            "message": "string",
-            "order_info": {
-              "id": null,
-              "id_tariff_type": 0,
-              "phone": "string",
-              "date_create": "2022-12-01T05:26:23.184Z",
-              "date_start": null,
-              "route_points": [
-                {
-                  "latitude": 0,
-                  "longitude": 0,
-                  "address": "string"
-                }
-              ],
-              "add_price": [
-                {
-                  "id": 0,
-                  "name": "string",
-                  "number": "string"
-                }
-              ],
-              "sum_noncash": 0,
-              "edit_count": 0,
-              "point_start": {
-                "latitude": 0,
-                "longitude": 0,
-                "address": "string"
-              },
-              "comment_start": "string",
-              "comment_order": "string",
-              "length": 0,
-              "empty_length": 0,
-              "length_out": 0,
-              "duration": 0,
-              "price": 0,
-              "free_wait_time": 0,
-              "need_time": 0,
-              "need_distance": 0,
-              "sum_add_prices_from_drv": 0,
-              "id_organization_order": 0
-            },
-            "driver_info": {
-              "id": "string",
-              "name": "string",
-              "nick_name": "string",
-              "phone": "string"
-            },
-            "auto_info": {
-              "model": "string",
-              "color": "string",
-              "number": "string"
-            },
-            "status": "New"
-          }';
-        $orderInfo = json_decode($orderObject);
-        return response()->json($orderInfo);
+        $getOrderObject = json_decode(file_get_contents('orderInfo.json'));
+        return response()->json([
+            "success"=> 0,
+            "message"=> "string",
+            "order_info" => $getOrderObject,
+            "driver_info"=>[
+                "id" => "string",
+                "name"=> "string",
+                "nick_name"=> "string",
+                "phone"=> "string"
+            ],
+              "auto_info" => [
+                "model"=> "string",
+                "color"=> "string",
+                "number"=> "string"
+        ],
+              "status" => "New"
+        ]);
 
     }
     public function createOrUpdate()
